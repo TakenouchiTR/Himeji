8f3847d73fd4808a4a17a535aa7d4829e6279665
Shawn Carter <totakenouchi@outlook.com>
2020-03-18 20:33:10 -0400

    Add a constructor and instance fields
    - This changes rendering from a static method to an instance method
    
    - Allows changes to the image from other classes

--- Himeji/src/MapImage.java
+++ Himeji/src/MapImage.java
@@ -11,6 +11,34 @@ import javax.imageio.ImageIO;
 
 public class MapImage 
 {
+	private int width;
+	private int height;
+	private File file;
+	private BufferedImage output;
+	
+	public MapImage(int width, int height, File outputFile)
+	{
+		this.width = width;
+		this.height = height;
+		this.file = outputFile;
+		
+		output = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+	}
+	
+	public void setPixel(int x, int y, int color)
+	{
+		output.setRGB(x, y, color);
+	}
+	
+	public void renderImage()
+	{
+		try
+		{
+			ImageIO.write(output, "png", file);
+		}
+		catch (Exception e) {}
+	}
+	
 	public static void renderDim(Dimension dim)
 	{
 		int width = dim.getBlockWidth();
@@ -53,4 +81,8 @@ public class MapImage
 		System.out.println((end.getTime() - start.getTime()) + "");
 		
 	}
+
+
+
+
 }
