1614143a44771489cf62b00e213a79b9db8e001f
Shawn Carter <totakenouchi@outlook.com>
2020-07-01 20:19:36 -0400

    Merge OldChunk and NewChunk into a single file

--- Himeji/src/com/takenouchitr/himeji/MCCompat/Chunk.java
+++ Himeji/src/com/takenouchitr/himeji/MCCompat/Chunk.java
@@ -20,13 +20,22 @@
 
 package com.takenouchitr.himeji.MCCompat;
 
+import com.mojang.nbt.ByteArrayTag;
 import com.mojang.nbt.CompoundTag;
+import com.mojang.nbt.IntArrayTag;
+import com.mojang.nbt.ListTag;
+import com.mojang.nbt.Tag;
+import com.takenouchitr.himeji.Himeji;
+import com.takenouchitr.himeji.Property;
 
-public abstract class Chunk 
+public class Chunk 
 {
 	public static final int CHUNK_SIZE = 16;
 	public static final int CHUNK_HEIGHT = 256;
 	
+	private String[][][] blocks;
+	private int[][][] biome;
+	private int biomeHeight;
 	protected CompoundTag levelTag;
 	
 	/**
@@ -36,23 +45,328 @@ public abstract class Chunk
 	 */
 	public Chunk(CompoundTag baseTag) 
 	{
+		blocks = new String[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE];
+		
 		levelTag = baseTag.getCompound("Level");
+		if (!baseTag.contains("DataVersion"))
+		{
+			readMcrChunk();
+			return;
+		}
+		
+		int version = baseTag.getInt("DataVersion");
+		
+		if (version < 1520)
+			readPreFlatChunk();
+		else
+			readPostFlatChunk();
 	}
 	
 	/**
-	 * Creates a three-dimensional int array of the ARGB values for the top-most blocks of 
-	 * each x, z and their y-value.
-	 * @param startY Y value to start searching at
-	 * @return int array of the colors for the top-most blocks of each x, z column
+	 * Reads a chunk using the McRegion format
 	 */
-	public abstract int[][][] getTopColors(int startY, int endY);
-
+	private void readMcrChunk()
+	{
+		biome = new int[CHUNK_SIZE][1][CHUNK_HEIGHT];
+		biomeHeight = CHUNK_HEIGHT;
+		
+		byte[] blockData = levelTag.getByteArray("Blocks");
+		byte[] metadataData = levelTag.getByteArray("Data");
+		int index = 0;
+		
+		for (int x = 0; x < CHUNK_SIZE; x++) 
+		{
+			for (int z = 0; z < CHUNK_SIZE; z++) 
+			{
+				for (int y = 0; y < 128; y++) 
+				{
+					int block = getPositiveByte(blockData[index]);
+					int meta = getHalfIndexValue(metadataData, index);
+					blocks[x][0][z] = Block.getNamespacedId(block, meta);
+					index++;
+					biome[x][0][z] = Biome.FOREST.id;
+				}
+			}
+		}
+		
+		for (int x = 0; x < CHUNK_SIZE; x++) 
+		{
+			for (int z = 0; z < CHUNK_SIZE; z++) 
+			{
+				for (int y = 128; y < CHUNK_HEIGHT; y++) 
+				{
+					blocks[x][0][z] = Block.getNamespacedId(0, 0);
+					biome[x][0][z] = Biome.FOREST.id;
+				}
+			}
+		}
+	}
+	
 	/**
-	 * Creates a three-dimensional int array of the ARGB values for the top-most blocks of 
-	 * each x, z and their y-value.
-	 * @return int array of the colors for the top-most blocks of each x, z column
+	 * Reads a chunk using the Anvil format, pre-flattening
 	 */
-	public abstract int[][][] getTopColors();
+	private void readPreFlatChunk()
+	{
+		biome = new int[CHUNK_SIZE][1][CHUNK_HEIGHT];
+		biomeHeight = CHUNK_HEIGHT;
+		
+		ListTag<? extends Tag> sections = levelTag.getList("Sections");
+		
+		//Fills all blocks with the default block namespace ID
+		for(int x = 0; x < CHUNK_SIZE; x++) 
+		{
+			for(int z = 0; z < CHUNK_SIZE; z++) 
+			{
+				for(int y = 0; y < CHUNK_HEIGHT; y++) 
+				{
+					blocks[x][y][z] = "minecraft:air";
+				}
+			}
+		}
+		
+		//Loads up the blocks stored in each of the sections
+		for(int i = 0; i < sections.size(); i++) 
+		{
+			CompoundTag section = (CompoundTag)sections.get(i);
+			
+			int sectionY = (int)(section.getByte("Y")) * 16;
+			if (sectionY < 0)
+				continue;
+			
+			byte[] blockData = section.getByteArray("Blocks");
+			byte[] metadataData = section.getByteArray("Data");
+			
+			//Skips the section if there is no block data
+			if (blockData.length == 0)
+				continue;
+			
+			for(int x = 0; x < CHUNK_SIZE; x++) 
+			{
+				for(int y = 0; y < CHUNK_SIZE; y++) 
+				{
+					for(int z = 0; z < CHUNK_SIZE; z++) 
+					{
+						int block = getPositiveByte(blockData[getIndex(x, y, z)]);
+						int meta = getHalfIndexValue(metadataData, x, y, z);
+						blocks[x][y + sectionY][z] = Block.getNamespacedId(block, meta);
+					}
+				}
+			}
+		}
+		
+		Tag biomeTag = levelTag.get("Biomes");
+		
+		//Loads the new, IntArray biome tag
+		if (biomeTag instanceof IntArrayTag)
+		{
+			int[] biomeData = levelTag.getIntArray("Biomes");
+			
+			for(int x = 0; x < CHUNK_SIZE; x++) 
+			{
+				for(int z = 0; z < CHUNK_SIZE; z++) 
+				{
+					biome[x][0][z] = biomeData[getIndex(x, z)];
+				}
+			}
+		}
+		//Loads the old, ByteArray biome tag
+		else if (biomeTag instanceof ByteArrayTag)
+		{
+			byte[] biomeData = levelTag.getByteArray("Biomes");
+			
+			for(int x = 0; x < CHUNK_SIZE; x++) 
+			{
+				for(int z = 0; z < CHUNK_SIZE; z++) 
+				{
+					biome[x][0][z] = getPositiveByte(biomeData[getIndex(x, z)]);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Reads a chunk using the Anvil format, post-flattening
+	 */
+	private void readPostFlatChunk()
+	{
+		Section[] sections = null;
+		
+		ListTag<? extends Tag> sectionTags = levelTag.getList("Sections");
+		
+		if (sectionTags.size() != 0)
+			sections = new Section[sectionTags.size() - 1];
+		
+		blocks = new String[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE];
+		
+		//Fills all blocks with the default block namespace ID
+		for(int x = 0; x < CHUNK_SIZE; x++) 
+		{
+			for(int z = 0; z < CHUNK_SIZE; z++) 
+			{
+				for(int y = 0; y < CHUNK_HEIGHT; y++) 
+				{
+					blocks[x][y][z] = "minecraft:air";
+				}
+			}
+		}
+		
+		//Stops if there is no block data stored in the tag
+		if (sections == null)
+			return;
+		
+		//Loads data from each of the sections
+		for(int i = 0; i < sections.length; i++) 
+		{
+			sections[i] = new Section((CompoundTag)sectionTags.get(i));
+			String[][][] sectionBlocks = sections[i].getBlocks();
+			int sectionY = (sections[i].getSectionTag().getByte("Y")) * 16;
+			
+			//Skips a section if there are no blocks stored
+			if (sectionBlocks == null)
+				continue;
+			
+			//Loads each of the blocks in the section
+			for(int x = 0; x < CHUNK_SIZE; x++) 
+			{
+				for(int y = 0; y < CHUNK_SIZE; y++) 
+				{
+					for(int z = 0; z < CHUNK_SIZE; z++) 
+					{
+						blocks[x][y + sectionY][z] = sectionBlocks[x][y][z];
+					}
+				}
+			}
+		}
+		
+		int dataLoc = 0;
+		int[] biomeData = levelTag.getIntArray("Biomes");
+		
+		//Loads new, 3D-Biomes (1.15+)
+		if (biomeData.length == 1024)
+		{
+			biome = new int[CHUNK_SIZE / 4][CHUNK_HEIGHT / 4][CHUNK_SIZE / 4];
+			biomeHeight = 4;
+			
+			for (int y = 0; y < CHUNK_HEIGHT / 4; y++)
+			{
+				for(int z = 0; z < CHUNK_SIZE / 4; z++)
+				{
+					for(int x = 0; x < CHUNK_SIZE / 4; x++) 
+					{	
+						biome[x][y][z] = biomeData[dataLoc];
+						dataLoc++;
+					}
+				}
+			}
+			
+		}
+		//Loads old, 2D-Biomes (pre 1.15)
+		else
+		{
+			biome = new int[CHUNK_SIZE][1][CHUNK_SIZE];
+			biomeHeight = CHUNK_HEIGHT;
+			
+			for(int z = 0; z < CHUNK_SIZE; z++) 
+			{
+				for(int x = 0; x < CHUNK_SIZE; x++) 
+				{
+					biome[x][0][z] = biomeData[dataLoc];
+					dataLoc++;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * reads a chunk using the Anvil format, post palette expansion
+	 */
+	@SuppressWarnings("unused")
+	private void readPostExpansionChunk()
+	{
+		
+	}
+	
+	/*
+	 * Returns the value of a byte as a positive integer.
+	 * @param b byte to convert
+	 * @return  integer of the positive form of the byte
+	 */
+	private int getPositiveByte(byte b) 
+	{
+		return (int)(b & 0xFF);
+	}
+	
+	/**
+	 * Gets the half of the value stored at the index of a byte array. The index 
+	 * is half of what the index is for a given block, rounded down. Even number
+	 * indices will give the last four bits, while odd will give the first four. 
+	 * @param data byte array containing data for all blocks in a section
+	 * @param x    x coordinate for the block
+	 * @param y    y coordinate for the block
+	 * @param z    z coordinate for the block
+	 * @return     half the value stored at the index
+	 */
+	private int getHalfIndexValue(byte[] data, int x, int y, int z) 
+	{
+		int result = 0;
+		int index = getIndex(x, y, z);
+		int halfIndex = index / 2;
+		boolean isEven = (index % 2) == 0;
+		byte b = data[halfIndex];
+		
+		if(isEven)
+		 	result = (int)(b & 0x0f);
+		else
+			result = (int)((b & 0xf0) >> 4);
+		
+		return result;
+	}
+	
+	private int getHalfIndexValue(byte[] data, int index) 
+	{
+		int result = 0;
+		int halfIndex = index / 2;
+		boolean isEven = (index % 2) == 0;
+		byte b = data[halfIndex];
+		
+		if(isEven)
+		 	result = (int)(b & 0x0f);
+		else
+			result = (int)((b & 0xf0) >> 4);
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the index for an array for an item stored at a chunk's x, y, z coordinate.
+	 * @param x x coordinate, relative to the chunk
+	 * @param y y coordinate
+	 * @param z z coordinate, relative to the chunk
+	 * @return  index of an array for the given coordinates
+	 */
+	private int getIndex(int x, int y, int z) 
+	{
+		int result;
+		
+		result = ((y * 16 + z) * 16 + x);
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the index for an array for an item stored at a chunk's x, z coordinate.
+	 * @param x x coordinate, relative to the chunk
+	 * @param z z coordinate, relative to the chunk
+	 * @return  index of an array for the given coordinates
+	 */
+	private int getIndex(int x, int z) 
+	{
+		int result;
+		
+		result = (z * 16 + x);
+		
+		return result;
+	}
 	
 	/**
 	 * Gets the chunk's x position for the current Chunk, relative to the entire world.
@@ -81,14 +395,70 @@ public abstract class Chunk
 		return levelTag;
 	}
 	
-	/**
+	/*
 	 * Gets the top block of a given x, z column that is not listed as invisible.
 	 * @param x       the Chunk's x coord, relative to the chunk
 	 * @param z       the Chunk's z coord, relative to the chunk
 	 * @param startY  the y coord to start searching at
 	 * @return        a color stored as an ARGB integer
 	 */
-	public abstract int getTopBlockY(int x, int z, int startY, int endY);
+	public int[][][] getTopColors(int startY, int endY)
+	{
+		int[][][] result = new int[CHUNK_SIZE][CHUNK_SIZE][2];
+		
+		if (blocks == null || biome == null)
+			return null;
+		
+		for (int x = 0; x < CHUNK_SIZE; x++)
+		{
+			for (int z = 0; z < CHUNK_SIZE; z++)
+			{
+				int y = getTopBlockY(x, z, startY, endY);
+				int dy = y;
+				if (Himeji.getProperty(Property.RENDER_UNDER_WATER).equals("true"))
+					dy = getTopBlockYIgnoreWater(x, z, startY, endY);
+				
+				int biomeWidth = CHUNK_SIZE / biome.length;
+				
+				int color = Block.getBlockColor(blocks[x][dy][z], 
+						biome[x / biomeWidth][dy / biomeHeight][z / biomeWidth]);
+				
+				//Adds a blue effect to blocks under water
+				if (y != dy)
+				{
+					int waterColor = Block.getBlockColor("minecraft:water", 
+							biome[x / biomeWidth][y / biomeHeight][z / biomeWidth]);
+					int a = 0;
+					int r = 0;
+					int g = 0;
+					int b = 0;
+					
+					a = 0xFF000000;
+					
+					r += ((color & 0x00FF0000) >>> 16);
+					r += ((waterColor & 0x00FF0000) >>> 16) * 3;
+					g += ((color & 0x0000FF00) >>> 8);
+					g += ((waterColor & 0x0000FF00) >>> 8) * 3;
+					b += (color & 0x000000FF);
+					b += (waterColor & 0x000000FF) * 3;
+					
+					r /= 4;
+					g /= 4;
+					b /= 4;
+					
+					r <<= 16;
+					g <<= 8;
+					
+					color = a | r | g | b;
+				}
+				
+				result[x][z][0] = color;
+				result[x][z][1] = dy;
+			}
+		}
+		
+		return result;
+	}
 	
 	/**
 	 * Gets the top block of a given x, z column that is not listed as invisible or is 
@@ -98,5 +468,61 @@ public abstract class Chunk
 	 * @param startY  the y coord to start searching at
 	 * @return        a color stored as an ARGB integer
 	 */
-	public abstract int getTopBlockYIgnoreWater(int x, int z, int startY, int endY);
+	public int[][][] getTopColors()
+	{
+		return getTopColors(CHUNK_HEIGHT - 1, 0);
+	}
+	
+	/**
+	 * Gets the top block of a given x, z column that is not listed as invisible.
+	 * @param x       the Chunk's x coord, relative to the chunk
+	 * @param z       the Chunk's z coord, relative to the chunk
+	 * @param startY  the y coord to start searching at
+	 * @return        a color stored as an ARGB integer
+	 */
+	public int getTopBlockY(int x, int z, int startY, int endY)
+	{
+		for(int y = startY; y >= endY; y--) 
+		{
+			String blockName = blocks[x][y][z];
+			
+			if (Block.isBlockVisible(blockName))
+			{
+				return y;
+			}
+		}
+		return 0;
+	}
+	
+	/**
+	 * Gets the top block of a given x, z column that is not listed as invisible or is 
+	 *   considered water.
+	 * @param x       the Chunk's x coord, relative to the chunk
+	 * @param z       the Chunk's z coord, relative to the chunk
+	 * @param startY  the y coord to start searching at
+	 * @return        a color stored as an ARGB integer
+	 */
+	public int getTopBlockYIgnoreWater(int x, int z, int startY, int endY)
+	{
+		for(int y = startY; y >= endY; y--) 
+		{
+			try
+			{
+				String blockName = blocks[x][y][z];
+				
+				if (Block.isBlockVisible(blockName) && 
+					!Block.hasWaterColor(blockName))
+				{
+					return y;
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
+		return 0;
+	}
+
 }
