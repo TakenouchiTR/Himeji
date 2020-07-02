b66014f77ebbc6da87f8b166251a4e2bf87f3b87
Shawn Carter <totakenouchi@outlook.com>
2020-07-02 15:32:47 -0400

    Add class to store properties for the render session

---
+++ Himeji/src/com/takenouchitr/himeji/SessionProperties.java
@@ -0,0 +1,15 @@
+package com.takenouchitr.himeji;
+
+public class SessionProperties
+{
+	public static float nightBrightness;
+	public static float biomeIntensity;
+	public static float waterTransparency;
+	public static float shadowIntensity;
+	public static boolean renderLight;
+	public static boolean renderBiomes;
+	public static boolean renderUnderWater;
+	public static boolean renderShadows;
+	
+	private SessionProperties() {}
+}
